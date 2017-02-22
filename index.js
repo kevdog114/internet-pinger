c0f68e1d851966ea8e8775330eaf6517d0116f89
Kevin Schaefer <kevin.l.schaefer1394@gmail.com>
2017-02-21 19:52:11 -0600

    trying different npm package to test connectivity

--- index.js
+++ index.js
@@ -1,16 +1,47 @@
 var tcpp = require("tcp-ping");
-
-//tcpp.probe("www.google.com", 81, function(err, available) {
-//  console.log(available);
-//});
+var sqlite3 = require("sqlite3").verbose();
+var db = new sqlite3.Database("results.db");
 
 var op = {
   address: "www.google.com",
-  port: 81,
+  port: 80,
   timeout: 5000,
-  attempts: 5
+  attempts: 3
 };
 
-tcpp.ping(op, function(err, data) {
-  console.log(data);
-});
+/*
+ *
+ * SQLite table schema
+ *
+ * CREATE TABLE ping_results (result_ms INT);
+ *
+ */
+
+console.log("Starting...");
+
+for(var i = 0; i < 10; i++) {
+	getResult(function(data) {
+		db.run("INSERT INTO ping_results VALUES ($ms);", {
+			$ms: data
+		});
+	});
+	
+}
+
+console.log("Finished");
+
+
+function getResult(whenDone) {
+	var promise = new Promise(function(resolve, reject) {
+		tcpp.ping(op, function(err, data) {
+			// if ping was unsuccessful, data.avg will be NaN
+			// else, data.avg will be average time in ms
+			if(data.avg === NaN) {
+				reject();
+			}
+			else {
+				resolve(data.avg);
+			}
+		});
+	});
+}
